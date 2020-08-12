d2015ffc1f20fc09cd499126c7940b280003db08
Lawrence-Krukrubo <sisokels@gmail.com>
2020-08-12 22:21:34 +0100

    Docs: daily coding exercise

--- august_2020/q_and-a.py
+++ august_2020/q_and-a.py
@@ -37,3 +37,89 @@ def uber_quest(arr):
 
 
 # print(uber_quest([1,2,3,4,5]))
+
+"""
+12th Aug
+
+Good morning! Here's your coding interview problem for today.
+This problem was asked by Google.
+Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, 
+and deserialize(s), which deserializes the string back into the tree.
+
+For example, given the following Node class
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+The following test should pass:
+
+node = Node('root', Node('left', Node('left.left')), Node('right'))
+assert deserialize(serialize(node)).left.left.val == 'left.left'
+"""
+# Ans: None Yet
+
+
+"""
+Implement atoi which converts a string to an integer.
+The function first discards as many whitespace characters as necessary until the first non-whitespace 
+character is found. Then, starting from this character, takes an optional initial plus or minus sign 
+followed by as many numerical digits as possible, and interprets them as a numerical value.
+The string can contain additional characters after those that form the integral number, 
+which are ignored and have no effect on the behavior of this function.
+If the first sequence of non-whitespace characters in str is not a valid integral number, 
+or if no such sequence exists because either str is empty or it contains only whitespace characters, 
+no conversion is performed.
+
+If no valid conversion could be performed, a zero value is returned.
+
+Note:
+
+Only the space character ' ' is considered as whitespace character.Assume we are dealing with an environment 
+which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. 
+If the numerical value is out of the range of representable values, 
+INT_MAX (231 − 1) or INT_MIN (−231) is returned.
+"""
+
+
+def myAtoi(str):
+    minn = -2**31
+    maxx = 2**31 - 1
+
+    try:
+        assert str
+    except AssertionError:
+        return 0
+
+    new_str = ''
+
+    for i in str:
+        if new_str:
+            if i.isdigit():
+                new_str += i
+                continue
+            else:
+                break
+        elif i in ['-', '+']:
+            new_str += i
+            continue
+        elif i.isdigit():
+            new_str += i
+        elif i != ' ':
+            return 0
+
+    try:
+        new_str = int(new_str)
+    except ValueError:
+        return 0
+
+    if new_str < minn:
+        return minn
+    elif new_str > maxx:
+        return maxx
+
+    return new_str
+
+#print(myAtoi(" b11228552307"))
