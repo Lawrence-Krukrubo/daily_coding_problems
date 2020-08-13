e01592d7ad96548946b9ff0e6723ac12bfc58fa5
Lawrence-Krukrubo <sisokels@gmail.com>
2020-08-13 23:39:03 +0100

    Update: 13th Aug exercise

--- august_2020/q_and-a.py
+++ august_2020/q_and-a.py
@@ -35,34 +35,12 @@ def uber_quest(arr):
 
     return y
 
-
 # print(uber_quest([1,2,3,4,5]))
 
-"""
-12th Aug
-
-Good morning! Here's your coding interview problem for today.
-This problem was asked by Google.
-Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, 
-and deserialize(s), which deserializes the string back into the tree.
-
-For example, given the following Node class
-
-class Node:
-    def __init__(self, val, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-        
-The following test should pass:
-
-node = Node('root', Node('left', Node('left.left')), Node('right'))
-assert deserialize(serialize(node)).left.left.val == 'left.left'
-"""
-# Ans: None Yet
-
 
 """
+12th Aug:
+
 Implement atoi which converts a string to an integer.
 The function first discards as many whitespace characters as necessary until the first non-whitespace 
 character is found. Then, starting from this character, takes an optional initial plus or minus sign 
@@ -123,3 +101,72 @@ def myAtoi(str):
     return new_str
 
 #print(myAtoi(" b11228552307"))
+
+
+"""
+13th Aug
+You are given a list of integers weights representing peoples' weights 
+and an integer limit representing the weight limit of one rocket ship.
+Each rocketship can take at most two people.
+Return the minimum number of rocket ships it would take to rescue everyone to Mars.
+
+Constraints
+
+Length of weights is at most 5000.
+Example 1
+Input
+
+weights = [200, 300, 200]
+limit = 400
+Output
+
+2
+Explanation:
+It would take one rocket ship to take the two people whose weights are 200, 
+and another to take the person whose weight is 300.
+"""
+
+
+def solve(weights, limit):
+
+    try:
+        assert weights
+    except AssertionError:
+        return 0
+
+    ships = 0
+
+    weights = sorted(weights, reverse=True)
+    counter = 0
+
+    for i in weights:
+        counter += 1
+        if i >= limit:
+            ships += 1
+            continue
+        elif weights.index(i) == len(weights)-1:
+            if i < limit:
+                ships += 1
+            break
+
+        is_lower = False
+        for j in weights[counter:]:
+            if i + j <= limit:
+                is_lower = True
+                ships += 1
+                weights.pop(weights.index(j))
+                break
+
+        if not is_lower:
+            ships += 1
+
+    return ships
+
+print(solve([200,300, 200], 400))
+
+
+
+
+
+
+
